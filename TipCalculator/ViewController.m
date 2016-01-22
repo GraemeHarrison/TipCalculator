d7afb0841e3ef96c1a17a6843f1112283ffd7177
Graeme Harrison <gharrison365@gmail.com>
2016-01-22 12:15:30 -0500

    Completed tasks 1, 2, and 3

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -9,14 +9,33 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UITextField *billAmountTextField;
+@property (strong, nonatomic) IBOutlet UILabel *tipAmountLabel;
 
 @end
 
 @implementation ViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.billAmountTextField resignFirstResponder];
+
+}
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    // Dismiss the keyboard whenever we touch outside the textfields by telling our textfields to give up first responder status
+    [self.billAmountTextField resignFirstResponder];
+}
+
+- (IBAction)calculateTip:(UIButton *)sender {
+    CGFloat num = [self.billAmountTextField.text integerValue];
+    CGFloat tip = num * 0.1500;
+    CGFloat roundedNum = ceilf(tip * 100.0) / 100.0;
+    
+    self.tipAmountLabel.text = [NSString stringWithFormat:@"Tip Amount: %.2f", roundedNum];
+    
 }
 
 - (void)didReceiveMemoryWarning {
